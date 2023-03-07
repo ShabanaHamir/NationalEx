4625a44d37d916d7e7a9f9422c247bc3ccc2a8a8
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-07 18:10:59 +0000

    added clsRoom

---
+++ Classes/clsRoom.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes
+{
+    public class clsRoom
+    {
+    }
+}
---
+++ Testing/tstRoom.cs
@@ -0,0 +1,18 @@
+﻿using Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class tstRoom
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create instance
+            clsRoom ARoom = new clsRoom();
+            Assert.IsNotNull(ARoom);
+        }
+    }
+}
