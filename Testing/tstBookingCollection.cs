f71517d1140e0d502345f7dc8d728b4192a8e810
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-07 20:26:53 +0000

    added empty test classes for hotel collection and booking ocllection

---
+++ Testing/tstBookingCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class tstBookingCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Testing/tstHotelCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class tstHotelCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
