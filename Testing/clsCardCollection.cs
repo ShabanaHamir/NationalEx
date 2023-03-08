9057b56e83d56a1e2211d36468242222ed43acb6
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-08 17:09:55 +0000

    added clsCard, clsCardCollection, clsPayment, clsConfirmation test classes

---
+++ Testing/clsCardCollection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class clsCardCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Testing/clsPayment.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class clsPayment
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
