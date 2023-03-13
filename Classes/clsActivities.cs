658ed075643ec84e7c2f5063417d86a7d29272ad
ShabanaHamir <P2618936@my365.dmu.ac.uk>
2023-03-13 19:28:08 +0000

    find method middle layer. no errors

--- Classes/clsActivities.cs
+++ Classes/clsActivities.cs
@@ -4,20 +4,39 @@ namespace Classes
 {
     public class clsActivities
     {
+        //private data member for acitivityname
+        private string mActivityName;
         public clsActivities()
         {
-        }
+        }   
+
 
         public bool Active { get; set; }
         public string ActivityCity { get; set; }
         public string ActivityDescription { get; set; }
-        public string ActivityName { get; set; }
+        public string ActivityName
+        {
+            get
+            {
+                //return priv data
+                return mActivityName;
+            }
+            set
+            {
+                //set priv data
+                mActivityName = value;
+            }
+        }
         public string ActivityPostCode { get; set; }
         public decimal ActivityPrice { get; set; }
 
         public bool Find(int activityID)
         {
+            //set priv data members to the test data value
+            mActivityName = "Kayaking";
+            //always return true
             return true;
         }
+        
     }
 }
