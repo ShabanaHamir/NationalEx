195aefc1c1aec2556539f8ec4037a0e7c86d548f
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-14 19:48:21 +0000

    added content place holder that is linked to the master page

--- FrontOffice/Default.Master.designer.cs
+++ FrontOffice/Default.Master.designer.cs
@@ -13,5 +13,14 @@ namespace FrontOffice
 
     public partial class Default1
     {
+
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
