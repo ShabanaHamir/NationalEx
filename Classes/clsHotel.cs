bbbb9214803432fc4cf4d6c02b3cb1b55de84bd2
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-16 18:50:24 +0000

    foind method to tstHotel and fixed the failing test

--- Classes/clsHotel.cs
+++ Classes/clsHotel.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace Classes
 {
@@ -18,27 +22,10 @@ namespace Classes
                 mHotelName = value;
             } 
         }
-        ////private member
-        //private string mHotelPostCode;
-        
+       
 
-        //private data member for the hottel id
+        //private data member for the hotel id
         private Int32 mHotelID;
-        //private member
-        private string mHotelPostCode;
-        public string HotelPostCode
-        {
-            get
-            {
-                return mHotelPostCode;
-            }
-            set
-            {
-                mHotelPostCode = value;
-            }
-        }
-
-
         //hotelid public property
         public int HotelID
         {
@@ -53,6 +40,23 @@ namespace Classes
                 mHotelID = value;
             }
         }
+        //private data member
+        public string mHotelPostCode;
+
+        public string HotelPostCode
+        {
+            get
+            {
+                //return the private data
+                return mHotelPostCode;
+            }
+            set
+            {
+                //set the private data
+                mHotelPostCode = value;
+            }
+        }
+
         public Boolean Valid(string Hotel)
         {
             return true;
@@ -63,6 +67,7 @@ namespace Classes
             //set private data memebers to the test data value
             mHotelID = 21;
             mHotelName = "Inn";
+            mHotelPostCode= "XXX XXX";
             //always true
             return true;
         }
