86f2dfddf0a02eca17bf5208bfaeda778145455e
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-20 23:57:06 +0000

    changes to Aroom.aspx and created roomviewer

---
+++ FrontOffice/ARoom.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FrontOffice
+{
+    public partial class ARoom : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+            //create a new instance of clsHotel
+            clsRoom ARoom = new clsRoom();
+            //capture
+            ARoom.HotelID = Convert.ToInt32(txtHotelID.Text);
+            //store the hotel in the session object
+            Session["ARoom"] = ARoom;
+            //redirect to the viewer page
+            Response.Redirect("RoomViewer.aspx");
+        }
+    }
+}
