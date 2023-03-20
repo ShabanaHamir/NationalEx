f385f81eb04a36a21c6fa4f174aac0a041e8f51a
ShabanaHamir <P2618936@my365.dmu.ac.uk>
2023-03-20 15:47:31 +0000

    activities progress

--- Classes/clsActivities.cs
+++ Classes/clsActivities.cs
@@ -6,13 +6,26 @@ namespace Classes
     {
         //private data member for acitivityname
         private string mActivityName;
+        private string mActivityCity;
         public clsActivities()
         {
         }   
 
 
         public bool Active { get; set; }
-        public string ActivityCity { get; set; }
+        public string ActivityCity
+        {
+            get
+            {
+                //return priv data
+                return mActivityCity;
+            }
+            set
+            {
+                //set priv data
+                mActivityCity = value;
+            }
+        }
         public string ActivityDescription { get; set; }
         public string ActivityName
         {
@@ -30,10 +43,11 @@ namespace Classes
         public string ActivityPostCode { get; set; }
         public decimal ActivityPrice { get; set; }
 
-        public bool Find(int activityID)
+        public bool Find(int ActivityID)
         {
             //set priv data members to the test data value
             mActivityName = "Kayaking";
+            mActivityCity = "Birmingham";
             //always true
             return true;
         }
