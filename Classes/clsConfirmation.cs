9fa5610640c35916c953f2e98fd68e001ac00a11
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-21 22:33:40 +0000

    added tests to tstConfirmation

---
+++ Classes/clsConfirmation.cs
@@ -0,0 +1,8 @@
+﻿namespace Classes
+{
+    public class clsConfirmation
+    {
+        public int CustomerID { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Testing/tstConfirmation.cs
+++ Testing/tstConfirmation.cs
@@ -1,14 +1,43 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-                                    //this class is shared among the system
+//this class is shared among the system
 namespace Testing
 {
     [TestClass]
     public class tstConfirmation
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InstanceOK()
         {
+            //create an instance of the class we want to create 
+            clsConfirmation AConfirmation = new clsConfirmation();
+            //test to see that it exists
+            Assert.IsNotNull(AConfirmation);
+        }
+        [TestMethod]
+        public void CustomerIDPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsConfirmation AConfirmation = new clsConfirmation();
+            //create some test data to assign to the property 
+            Int32 TestData = 2;
+            //assign the data to the property
+            AConfirmation.CustomerID = TestData;
+            //test to see that it exists
+            Assert.AreEqual(AConfirmation.CustomerID, TestData);
+        }
+        [TestMethod]
+        public void EmailPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsConfirmation AConfirmation = new clsConfirmation();
+            //create some test data to assign to the property 
+            string TestData = "XXXX@xxx.com";
+            //assign the data to the property
+            AConfirmation.Email= TestData;
+            //test to see that it exists
+            Assert.AreEqual(AConfirmation.Email, TestData);
         }
     }
 }
