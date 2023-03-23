310688051a4c9b21c5661f232426913b336e8b0c
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-23 00:34:14 +0000

    added find method to presentation layer

--- FrontOffice/Payment.aspx.cs
+++ FrontOffice/Payment.aspx.cs
@@ -31,5 +31,30 @@ namespace FrontOffice
         {
 
         }
+
+        protected void btnFind_Click(object sender, EventArgs e)
+        {
+            //create a new instance of clsHotel
+            clsPayment APayment = new clsPayment();
+            //var to store the PK
+            Int32 PaymentID;
+            //var to store the result of the find operation
+            Boolean Found = false;
+            //get the PK entered by the user
+            PaymentID = Convert.ToInt32(txtPaymentID.Text);
+            //find record
+            Found = APayment.Find(PaymentID);
+            //if found
+            if (Found == true)
+            {
+                //display values of the properties in the form
+                txtCustomerID.Text = APayment.CustomerID.ToString();
+                txtPaymentAmount.Text = APayment.PaymentAmount.ToString();
+                txtPaymentDate.Text = APayment.PaymentDate.ToString();
+                txtPaymentType.Text = APayment.PaymentType;
+
+
+            }
+        }
     }
 }
