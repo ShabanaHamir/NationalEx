c12e4103a984032302714b0559a02ff3ffc961e7
p2625596 <p2625596@GWHXX651WIN010W>
2023-03-26 23:02:42 +0100

    added sproc to data layer

--- FrontOffice/Default.aspx.designer.cs
+++ FrontOffice/Default.aspx.designer.cs
@@ -13,23 +13,5 @@ namespace FrontOffice
 
     public partial class Default
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
