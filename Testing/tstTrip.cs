12e1f94a52e98044c95d0b2269387aea76047268
Sanad Eid <P2625392@my365.dmu.ac.uk>
2023-04-10 13:02:21 +0100

    all good

---
+++ Testing/tstTrip.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing
+{
+    [TestClass]
+    public class tstTrip
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create 
+            clsTrip AnTrip = new clsTrip();
+            //test to see that it exists 
+            Assert.IsNotNull(AnTrip);
+
+        }
+    }
+
+    class clsTrip
+    {
+        public clsTrip()
+        {
+        }
+    }
+}
