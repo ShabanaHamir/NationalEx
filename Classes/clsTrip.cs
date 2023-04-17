d745cf018a77101d530462f2e64732fab04d1721
Sanad Eid <P2625392@my365.dmu.ac.uk>
2023-04-17 23:58:15 +0100

    bueno

--- Classes/clsTrip.cs
+++ Classes/clsTrip.cs
@@ -4,9 +4,78 @@ namespace Classes
 {
     public class clsTrip
     {
-        public int TripID { get; set; }
-        public DateTime TripDate { get; set; }
-        public string TripDestination { get; set; }
-        public string VehicleType { get; set; }
+        //create the trip ID private variable 
+        private Int32 aTripID;
+        //create tripID Public property 
+        public Int32 TripID
+        {
+            get
+            {
+                //transfer data oyt of the property 
+                return aTripID;
+            }
+            set
+            {
+                aTripID = value;
+            }
+        }
+        //tripDate set to be private variable 
+        private DateTime aTripDate;
+        //trip date set to be public property 
+        public DateTime TripDate
+        {
+            get
+            {
+                return aTripDate;
+            }
+            set
+            {
+                aTripDate = value;
+
+            }
+        }
+
+        //tripDate set to be private variable 
+        private string aTripDestination;
+        //trip destination set it public 
+        public string TripDestination
+        {
+            get
+            {
+                return aTripDestination;
+            }
+            set
+            {
+                aTripDestination = value;
+
+            }
+        }
+
+        //tripDate set to be private variable 
+        private string aVehicleType;
+        //trip destination set it public 
+        public string VehicleType
+        {
+            get
+            {
+                return aVehicleType;
+            }
+            set
+            {
+                aVehicleType = value;
+
+            }
+        }
+
+        public bool Find(int tripID)
+        {
+            //set the private variable to test the value 
+            aTripID = 11;
+            aTripDate = Convert.ToDateTime("04/04/2023");
+            aTripDestination = "London";
+            aVehicleType = "Bus";
+            // return the value true 
+            return true;
+        }
     }
 }
