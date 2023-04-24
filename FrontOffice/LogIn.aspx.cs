e3e83b66e95aea129e5a0dfe2225e8337fe293b1
ShabanaHamir <P2618936@my365.dmu.ac.uk>
2023-04-24 19:10:17 +0100

    redirect loop fixed

--- FrontOffice/LogIn.aspx.cs
+++ FrontOffice/LogIn.aspx.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.UI;
+//using System.Web.UI.WebControls;
 
-namespace FrontOffice
-{
-    public partial class WebForm1 : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+//namespace FrontOffice
+//{
+//    public partial class WebForm1 : System.Web.UI.Page
+//    {
+//        protected void Page_Load(object sender, EventArgs e)
+//        {
 
-        }
+//        }
 
-        protected void btnLogin_Click(object sender, EventArgs e)
-        {
+//        protected void btnLogin_Click(object sender, EventArgs e)
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
--- FrontOffice/SignUp.aspx.cs
+++ FrontOffice/SignUp.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace FrontOffice
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class SignUp : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Redirect("AccountActivation.aspx");
+            //Response.Redirect("AccountActivation.aspx");
         }
 
         protected void btnSignup_Click(object sender, EventArgs e)
