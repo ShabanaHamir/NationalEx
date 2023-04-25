98374e1b2c38584308df24b1b05b1db53550aa59
Sanad Eid <P2625392@my365.dmu.ac.uk>
2023-04-25 17:57:48 +0100

    worked on trip

--- Testing/tstTrip.cs
+++ Testing/tstTrip.cs
@@ -1,12 +1,22 @@
 ﻿using Classes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Class_Library;
 using System;
 
 namespace Testing
 {
+
+
     [TestClass]
     public class tstTrip
     {
+        //good test data 
+        //some test data to pass the method 
+        string TripID = "2";
+        string TripDate = DateTime.Now.Date.ToString();
+        string TripDestination = "Paris";
+            string VehicleType ="Coach";
+
         [TestMethod]
         public void InstanceOK()
         {
@@ -177,6 +187,24 @@ namespace Testing
             //test to see the result
             Assert.IsTrue(OK); //
         }
+
+
+        [TestMethod]
+        public void ValidMethodOK ()
+        {
+            //create an instance of the class
+            clsTrip AnTrip = new clsTrip();
+            //string variable to store any error message 
+            String Error = "";
+            //invoke the method 
+            Error = AnTrip.Valid(TripID,TripDate,TripDestination,VehicleType);
+            //test to see that the result is correct 
+            Assert.AreEqual(Error, "");
+        }
+
+
+
+
 
 
 
