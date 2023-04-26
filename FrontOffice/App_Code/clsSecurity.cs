06b43dba634c0a40af46fe31bf9faf1c87802add
p2625596 <p2625596@GWHXX651WIN010W>
2023-04-26 12:38:42 +0100

    App_code in frontoffice

---
+++ FrontOffice/App_Code/clsSecurity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class clsSecurity
+    {
+        public class clsEmail
+        {
+
+            //this class is internal to clsSecurity for easy use
+            public string Subject { get; internal set; }
+            public string Recipient { get; internal set; }
+            public string Sender { get; internal set; }
+            public string Body { get; internal set; }
+
+            public void SendEmail()
+            {
+                //send email
+                System.Net.Mail.MailMessage eMail = new System.Net.Mail.MailMessage();
+                System.Net.Mail.SmtpClient Server = new System.Net.Mail.SmtpClient("mail.nationalexperience.uk");
+                eMail = new System.Net.Mail.MailMessage(this.Sender, this.Recipient, this.Subject, this.Body);
+                //Server.Send(email);
+            }
+
+        }
+
+        public string SignUp(string text1, string text2, string text3, bool v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
