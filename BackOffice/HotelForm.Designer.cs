61ecd043cd1ef9268b824fe33d3223fabb72ff2d
p2625596 <p2625596@GWHXX651WIN010W>
2023-04-26 17:06:59 +0100

    added hotelForm in backoffice

---
+++ BackOffice/HotelForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace BackOffice
+{
+    partial class HotelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HotelID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hotel Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hotel Phone Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hotel Post Code:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(170, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(170, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 7;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(305, 290);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 8;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(169, 290);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 9;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(476, 247);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 15);
+            this.lblError.TabIndex = 10;
+            // 
+            // HotelForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "HotelForm";
+            this.Text = "HotelForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ BackOffice/HotelForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Classes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackOffice
+{
+    public partial class HotelForm : Form
+    {
+        private TextBox txtHotelID;
+        private TextBox txtHotelName;
+        private TextBox txtHotelPhoneNumber;
+        private TextBox txtHotelPostCode;
+        private Button btnOK;
+        private Button btnFind;
+        private Label lblError;
+
+        public HotelForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    this.txtHotelID = new System.Windows.Forms.TextBox();
+        //    this.txtHotelName = new System.Windows.Forms.TextBox();
+        //    this.txtHotelPhoneNumber = new System.Windows.Forms.TextBox();
+        //    this.txtHotelPostCode = new System.Windows.Forms.TextBox();
+        //    this.btnOK = new System.Windows.Forms.Button();
+        //    this.btnFind = new System.Windows.Forms.Button();
+        //    this.lblError = new System.Windows.Forms.Label();
+        //    this.SuspendLayout();
+        //    // 
+        //    // txtHotelID
+        //    // 
+        //    this.txtHotelID.Location = new System.Drawing.Point(12, 12);
+        //    this.txtHotelID.Name = "txtHotelID";
+        //    this.txtHotelID.Size = new System.Drawing.Size(100, 20);
+        //    this.txtHotelID.TabIndex = 0;
+        //    // 
+        //    // txtHotelName
+        //    // 
+        //    this.txtHotelName.Location = new System.Drawing.Point(12, 38);
+        //    this.txtHotelName.Name = "txtHotelName";
+        //    this.txtHotelName.Size = new System.Drawing.Size(100, 20);
+        //    this.txtHotelName.TabIndex = 1;
+        //    // 
+        //    // txtHotelPhoneNumber
+        //    // 
+        //    this.txtHotelPhoneNumber.Location = new System.Drawing.Point(12, 64);
+        //    this.txtHotelPhoneNumber.Name = "txtHotelPhoneNumber";
+        //    this.txtHotelPhoneNumber.Size = new System.Drawing.Size(100, 20);
+        //    this.txtHotelPhoneNumber.TabIndex = 2;
+        //    // 
+        //    // txtHotelPostCode
+        //    // 
+        //    this.txtHotelPostCode.Location = new System.Drawing.Point(12, 90);
+        //    this.txtHotelPostCode.Name = "txtHotelPostCode";
+        //    this.txtHotelPostCode.Size = new System.Drawing.Size(100, 20);
+        //    this.txtHotelPostCode.TabIndex = 3;
+        //    // 
+        //    // btnOK
+        //    // 
+        //    this.btnOK.Location = new System.Drawing.Point(12, 116);
+        //    this.btnOK.Name = "btnOK";
+        //    this.btnOK.Size = new System.Drawing.Size(75, 23);
+        //    this.btnOK.TabIndex = 4;
+        //    this.btnOK.Text = "OK";
+        //    this.btnOK.UseVisualStyleBackColor = true;
+        //    this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+        //    // 
+        //    // btnFind
+        //    // 
+        //    this.btnFind.Location = new System.Drawing.Point(93, 116);
+        //    this.btnFind.Name = "btnFind";
+        //    this.btnFind.Size = new System.Drawing.Size(75, 23);
+        //    this.btnFind.TabIndex = 5;
+        //    this.btnFind.Text = "Find";
+        //    this.btnFind.UseVisualStyleBackColor = true;
+        //    this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+        //    // 
+        //    // lblError
+        //    // 
+        //    this.lblError.AutoSize = true;
+        //    this.lblError.Location = new System.Drawing.Point(12, 142);
+        //    this.lblError.Name = "lblError";
+        //    this.lblError.Size = new System.Drawing.Size(0, 13);
+        //    this.lblError.TabIndex = 6;
+        //    // 
+        //    // HotelForm
+        //    // 
+
+    }
+    }
