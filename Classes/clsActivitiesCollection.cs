60ee9ef30f1e614a08917b0d5e917192f45cd3f4
ShabanaHamir <P2618936@my365.dmu.ac.uk>
2023-05-02 20:53:53 +0100

    23 Lab page 8

--- Classes/clsActivitiesCollection.cs
+++ Classes/clsActivitiesCollection.cs
@@ -1,6 +1,10 @@
-﻿namespace Classes
+﻿using System.Collections.Generic;
+
+namespace Classes
 {
     public class clsActivitiesCollection
     {
+        public List<clsActivities> ActivitiesList { get; set; }
+        public clsActivities ThisActivity { get; set; }
     }
 }
