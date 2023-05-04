1e68f0f9d0958cf94907ee86d3aa77ade4e2f398
ShabanaHamir <P2618936@my365.dmu.ac.uk>
2023-05-04 16:43:28 +0100

    23 lab completed (mostly) re check last few pages

--- FrontOffice/ActivityDefault.aspx.cs
+++ FrontOffice/ActivityDefault.aspx.cs
@@ -15,22 +15,43 @@ namespace FrontOffice
             if (IsPostBack == false)
             {
                 //update list box
-                DisplayActivities();
+                DisplayActivityNames();
             }
         }
 
-        void DisplayActivities()
+        void DisplayActivityNames()
+        {
+            {
+                //create an imstance of the activities
+                clsActivitiesCollection Activities = new clsActivitiesCollection();
+                //set the data source to the list of activities in the collection
+                lstActivities.DataSource = Activities.ActivitiesList;
+                //set the name of the PK
+                lstActivities.DataValueField = "ActivityID";
+                //set data field to display 
+                lstActivities.DataTextField = "Activity Name";
+                //bind the data to the list
+                lstActivities.DataBind();
+            }
+        }
+
+        Int32 DisplayActivities(string ActivityNameFilter)
         {
             //create an imstance of the activities
-            Classes.clsActivitiesCollection Activities = new Classes.clsActivitiesCollection();
-            //set the data source to the list of activities in the collection
-            lstActivities.DataSource = Activities.ActivitiesList;
-            //set the name of the PK
+            clsActivitiesCollection MyActivities = new clsActivitiesCollection();
+            //call the filter by activity name 
+            MyActivities.FilterByActivityName(ActivityNameFilter);
+            
+            //point the list box at the activitylist collection
+            lstActivities.DataSource = MyActivities.ActivityList;
+            //tell it what field to display to user
+            lstActivities.DataTextField = "ActivitiyName";
+            //set pk value
             lstActivities.DataValueField = "ActivityID";
-            //set data field to display 
-            lstActivities.DataTextField = "Activity Name";
-            //bind the data to the list
+            //bind the data to the list and display it
             lstActivities.DataBind();
+            //return no of records found
+            return MyActivities.Count;
         }
     }
 }
