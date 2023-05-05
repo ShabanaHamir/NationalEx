58c7786656f3ae4a36e2b45005632dcbc5387a35
p2625596 <p2625596@GWHXX651WIN010W>
2023-05-05 16:29:45 +0100

    tstHotelCollection tests

--- Testing/tstHotelCollection.cs
+++ Testing/tstHotelCollection.cs
@@ -25,12 +25,13 @@ namespace Testing
             List<clsHotel> TestList = new List<clsHotel>();
             //add an item to the list
             //create item of the test data
-            clsHotel TestItem =new clsHotel();
+            clsHotel TestItem = new clsHotel();
             //set its properties
             TestItem.HotelID = 1;
-            TestItem.HotelName = "Inn";
-            TestItem.HotelPhoneNumber = 01234567891;
-            TestItem.HotelPostCode = "XXX XXX";
+            TestItem.HotelName = "Holiday Inn";
+            TestItem.HotelCity = "Leicester";
+            TestItem.HotelPhoneNumber = 23092002;
+            TestItem.HotelPostCode = "LE4 8UE";
             //add item to the test list
             TestList.Add(TestItem);
             //assign data to property
@@ -39,14 +40,14 @@ namespace Testing
             Assert.AreEqual(AllHotels.HotelList, TestList);
 
         }
-        [TestMethod]
-        public void CountPropertyOK()
-        {
-            clsHotelCollection AllHotels = new clsHotelCollection();
-            Int32 SomeCount = 0;
-            AllHotels.Count = SomeCount;
-            Assert.AreEqual(AllHotels.Count, SomeCount);
-        }
+        //[TestMethod]
+        //public void CountPropertyOK()
+        //{
+        //    clsHotelCollection AllHotels = new clsHotelCollection();
+        //    Int32 SomeCount = 2;
+        //    AllHotels.Count = SomeCount;
+        //    Assert.AreEqual(AllHotels.Count, SomeCount);
+        //}
         [TestMethod]
         public void ThisHotelPropertyOK()
         {
@@ -54,13 +55,48 @@ namespace Testing
             clsHotel TestHotel = new clsHotel();
             //set properties of the test object
             TestHotel.HotelID = 1;
-            TestHotel.HotelName = "Inn";
-            TestHotel.HotelPhoneNumber = 01234567891;
-            TestHotel.HotelPostCode = "XXX XXX";
+            TestHotel.HotelName = "Holiday Inn";
+            TestHotel.HotelCity = "Leicester";
+            TestHotel.HotelPhoneNumber = 23092002;
+            TestHotel.HotelPostCode = "LE4 8UE";
             //assign data to the property
             AllHotels.ThisHotel = TestHotel;
             Assert.AreEqual(AllHotels.ThisHotel, TestHotel);
 
         }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsHotelCollection AllHotels = new clsHotelCollection();
+            //create test data to assign to proprty
+            //list of objrcts for the data
+            List<clsHotel> TestList = new List<clsHotel>();
+            //test data
+            clsHotel TestItem = new clsHotel();
+            //set properties
+            //set its properties
+            TestItem.HotelID = 1;
+            TestItem.HotelName = "Holiday Inn";
+            TestItem.HotelCity = "Leicester";
+            TestItem.HotelPhoneNumber = 23092002;
+            TestItem.HotelPostCode = "LE4 8UE";
+            //add the item to test list
+            TestList.Add(TestItem);
+            //assign data to property
+            AllHotels.HotelList = TestList;
+            //test that two values are the same
+            Assert.AreEqual(AllHotels.Count, TestList.Count);
+
+        }
+
+        //[TestMethod]
+        //public void TwoRecordsPresent()
+        //{
+        //    clsHotelCollection AllHotels = new clsHotelCollection();
+        //    Assert.AreEqual(AllHotels.Count, 2);
+        //}
+
+
     }
 }
