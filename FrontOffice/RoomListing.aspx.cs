1201769fd5e39e11f6dadb5b2bc6ec2bbc2a3706
p2625596 <p2625596@GWHXX651WIN010W>
2023-05-16 18:28:19 +0100

    activitiy booking address and ROOMLISING WORKSSSSSSSSS

---
+++ FrontOffice/RoomListing.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using Classes;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace FrontOffice
+{
+    public partial class RoomListing : System.Web.UI.Page
+    {
+        //protected Repeater roomRepeater;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                if (!IsPostBack)
+                {
+                    BindRoomData();
+                }
+        }
+
+            private void BindRoomData()
+            {
+                    clsRoomCollection roomCollection = new clsRoomCollection();
+                    List<clsRoom> rooms = roomCollection.GetAllRooms();
+
+                    roomRepeater.DataSource = rooms;
+                    roomRepeater.DataBind();
+            }
+
+            protected void addToCartButton_Click(object sender, EventArgs e)
+            {
+                // Retrieve the RoomId from the CommandArgument of the clicked button and perform the necessary logic.
+                // You can add the room to the cart or redirect to a different page, depending on your requirements.
+            }
+       }
+    
+}
